0bd0fa20eaf1b482471d627e85fcaaddc2ac3469
Jos Warmer <jos.warmer@openmodeling.nl>
2020-04-24 18:42:12 +0200

    Generation of ProjectIt language definition files

--- mps-analyser/src/main/kotlin/nl/dslconsultancy/mps/analyser/generator.kt
+++ mps-analyser/src/main/kotlin/nl/dslconsultancy/mps/analyser/generator.kt
@@ -48,6 +48,7 @@ fun GenerateFromStructure.run(mpsProjectOnDisk: MpsProjectOnDisk) {
     val genPath = Paths.get(generationPath)
     writeJson(structure, genPath/"export-${language.name}.json")
     Files.write(genPath/"kotlin-${language.name}.kt", generateKotlinFor(structure))
+    Files.write(genPath/"projectit-${language.name}.lang", generateProjectItFor(languageName, structure))
     Files.write(
         genPath/"structure-${language.name}.csv",
         listOf(csvRowOf("\"concept[#feature]\"", "deprecated")) + generateCsvFor(structure).sorted()
@@ -55,3 +56,69 @@ fun GenerateFromStructure.run(mpsProjectOnDisk: MpsProjectOnDisk) {
     println("wrote \"stuff\" generated for structure of '${language.name}' to '$genPath'")
 }
 
+////////////////////////////////
+// ProjectIt stuff
+///////////////////////////////
+fun generateProjectItFor(languageName: String, structure: Structure): Iterable<String> =
+    listOf("language $languageName\n").plus(
+        structure.concepts().flatMap {structuralElement ->  structuralElement.generateProjectItFor() }).plus(
+        structure.enumerations().flatMap {enumeration ->  enumeration.generateProjectItFor() })
+
+private fun Concept.superConcepts(): List<Concept> =
+    (if (extends == null) emptyList() else listOf(extends!!))
+
+private fun Concept.superInterfaces(): List<Concept> =
+    (if (true) emptyList() else listOf(extends!!)) + implements
+
+private fun Link.listMarker(): String =
+    (if (cardinality == "0..n" || cardinality == "1..n") "[]" else "")
+
+private fun Property.type(): String =
+    // Might want to check whether the constrained datatype exists
+    (if (listOf("string", "boolean", "integer").contains(targetType) ) "$targetType" else "string")
+
+
+
+private fun StructuralElement.generateProjectItFor(): Iterable<String> =
+    when (this) {
+        is Concept -> {
+            when {
+                isInterface -> {
+                    val supers = superInterfaces()
+                    listOf(
+                        "interface $name" +(if (supers.isEmpty()) "" else " base " + supers.map { it.name }.joinToString(", ")) + " {",
+                        features.filterIsInstance<Property>().joinToString(",\n") {property ->  "\t${property.name}: ${property.targetType}" },
+                        "}" ,
+                        ""
+                    )
+                }
+                else -> {
+                    val superConcepts = superConcepts()
+                    val superInterfaces = superInterfaces()
+                    listOf(
+                        "concept $name " + (if (superConcepts.isEmpty()) "" else " base " + superConcepts.map { it.name }.joinToString(", ")) +
+                            (if (superInterfaces.isEmpty()) "" else " implements " + superInterfaces.map { it.name }.joinToString(", ")) + " {",
+                        features.filterIsInstance<Property>().joinToString("\n") {property ->
+                            "\t${property.name}: ${property.type()};"},
+                        features.filterIsInstance<Link>().joinToString("\n") {link ->
+                            (if (link.reference) "\treference " else "\t") + "${link.name}: ${link.targetType}" + link.listMarker() + ";" },
+                        "}" ,
+                        ""
+                    )
+                }
+            }
+        }
+    }
+
+private fun Enumeration.generateProjectItFor(): Iterable<String> =
+    when (this) {
+        is Enumeration -> {
+                    listOf(
+                        "enumeration $name {",
+                        members.joinToString(",\n") {enumerationMember ->  "\t${enumerationMember.name}" },
+                        "}" ,
+                        ""
+                    )
+                }
+        else -> { listOf("") }
+    }
